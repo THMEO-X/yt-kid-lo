b91e686762a5980eb0b4d3854127e823a66c46ce
THMEO-X <htgaming7811@gmail.com>
2026-06-12 22:13:14 +0700

    Add files via upload

--- webhock.js
+++ webhock.js
@@ -12,12 +12,15 @@ async function uploadImage(imageUrl) {
         console.log("[NEW]", imageUrl);
 
         const image = await axios.get(
-            imageUrl,
-            {
-                responseType: "arraybuffer"
-            }
-        );
-
+    imageUrl,
+    {
+        responseType: "arraybuffer",
+        headers: {
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36"
+        }
+    }
+);
         const form = new FormData();
 
         form.append(
